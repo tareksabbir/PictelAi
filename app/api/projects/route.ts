262ea56ec82d2e258b1d9f559986b995b7d478c0
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-10-08 22:43:24 +0600

    Add POST endpoint for project creation
    Implements a new API route to handle project creation, frame association, and chat message saving. The endpoint inserts records into the project, frame, and chat tables using data from the request and the current user.

---
+++ app/api/projects/route.ts
@@ -0,0 +1,26 @@
+import db from "@/config/db";
+import { chatTable, frameTable, projectTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { projectId, frameId, messages } = await req.json();
+  const user = await currentUser();
+  // create projects
+  const projectResult = await db.insert(projectTable).values({
+    projectId,
+    createdBy: user?.primaryEmailAddress?.emailAddress,
+  });
+  // create frames
+  const frameResult = await db.insert(frameTable).values({
+    frameId,
+    projectId,
+  });
+  // save user chats
+
+  const chatResult = await db.insert(chatTable).values({
+    chatMessage: messages,
+    createdBy: user?.primaryEmailAddress?.emailAddress,
+  });
+   return NextResponse.json({ projectResult, frameResult, chatResult });
+}
