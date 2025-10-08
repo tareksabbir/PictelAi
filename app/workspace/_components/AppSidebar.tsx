e9dccb9d4ba47c5efc1d3374807b69c118a7016c
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-10-08 12:57:48 +0600

    Add AppSidebar component for workspace UI
    Introduces the AppSidebar component, including project listing, logo, add new project button, and footer section for the workspace area.

---
+++ app/workspace/_components/AppSidebar.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import ProjectList from "./ProjectList";
+import SidebarFooterSection from "./SidebarFooterSection";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="flex items-center gap-2 p-4">
+          <Image src="/logo.svg" alt="Logo" width={140} height={50} />
+        </div>
+        <Link href={"/workspace/new"} className="mt-5 w-full px-4">
+          <Button className="w-full"> + Add New Project</Button>
+        </Link>
+      </SidebarHeader>
+      <SidebarContent className="p-4">
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <ProjectList />
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter >
+        <SidebarFooterSection />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
