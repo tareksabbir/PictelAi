ced3e0e96c86e276e6ab47a7523ac54f623fb545
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-10-09 06:35:47 +0600

    Add ChatSection component for playground
    Introduces a new ChatSection component that accepts a list of messages as props and renders a styled section. This sets up the structure for displaying chat messages in the playground feature.

---
+++ app/playground/_components/ChatSection.tsx
@@ -0,0 +1,15 @@
+import { Messages } from "../[projectId]/page";
+
+type Props = {
+    messages: Messages[]
+};
+
+const ChatSection = ({messages}:Props) => {
+  return (
+    <section className="w-96 shadow h-[95vh] p-4">
+
+    </section>
+  )
+}
+
+export default ChatSection
