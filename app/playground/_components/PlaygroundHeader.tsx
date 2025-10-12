68ed0c2ec9c453d63d0f351f94ea319e0918dc38
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-10-12 16:31:11 +0600

    Update PlaygroundHeader.tsx

--- app/playground/_components/PlaygroundHeader.tsx
+++ app/playground/_components/PlaygroundHeader.tsx
@@ -1,15 +1,20 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { OnSaveContext } from "@/context/OnSaveContext";
 import Image from "next/image";
+import Link from "next/link";
+import { useContext } from "react";
 
 const PlaygroundHeader = () => {
+  const { onSaveData, setOnSaveData } = useContext(OnSaveContext);
   return (
     <header className="flex items-center justify-between p-4 shadow-md">
-      <Image src="/logo.svg" alt="Logo" width={140} height={50} />
-      <Button>Save</Button>
+      <Link href={"/"}>
+        <Image src="/logo.svg" alt="Logo" width={140} height={50} />
+      </Link>
+      <Button onClick={() => setOnSaveData(Date.now())}>Save</Button>
     </header>
   );
 };
 
 export default PlaygroundHeader;
-
-
