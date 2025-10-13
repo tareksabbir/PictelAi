eeb04eab8ce42a634743a5f246fdccc343d0f11c
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-10-13 12:41:09 +0600

    Added the pricing page route here

---
+++ app/workspace/pricing/page.tsx
@@ -0,0 +1,23 @@
+import { PricingTable } from "@clerk/nextjs";
+
+const Pricing = () => {
+  return (
+    <section className="my-10">
+      <div>
+        <h2 className="text-2xl font-bold text-center mb-3">
+          Choose your plan
+        </h2>
+        <p className=" text-sm text-gray-500 text-center mb-10">
+          {" "}
+          Export clean, optimized code with accessibility features and
+          responsive design built-in
+        </p>
+      </div>
+      <div style={{ maxWidth: "800px", margin: "0 auto", padding: "0 1rem" }}>
+        <PricingTable />
+      </div>
+    </section>
+  );
+};
+
+export default Pricing;
